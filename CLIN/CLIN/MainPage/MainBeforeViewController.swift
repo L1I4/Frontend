e3dad257b7c3e012960057aaeb56a549a50d46db
DDING <102645399+OhMyungJin@users.noreply.github.com>
2023-06-29 11:39:34 +0900

    main_3/추가 논의 필요

--- CLIN/CLIN/MainPage/MainBeforeViewController.swift
+++ CLIN/CLIN/MainPage/MainBeforeViewController.swift
@@ -10,4 +10,8 @@ import UIKit
 class MainBeforeViewController: UIViewController {
     
     @IBOutlet weak var goTiket: UIButton!
+    
+    @IBAction func touchGoTiket() {
+        // 나의 티켓으로 이동
+    }
 }
