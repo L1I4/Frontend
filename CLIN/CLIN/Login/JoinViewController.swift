c128373720c11f341f3545c67206041f498d536f
YoonSub Lim <yslim37@naver.com>
2023-06-29 21:15:59 +0900

    Add: 본인인증 화면 이미지 추가 및 ViewController 분할

--- CLIN/CLIN/Login/JoinViewController.swift
+++ CLIN/CLIN/Login/JoinViewController.swift
@@ -8,30 +8,17 @@
 import UIKit
 
 class JoinViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var phoneNumberTextField: UITextField!
-    
-    
+
     @IBAction func certificationBtnTap(_ sender: Any) {
         
-        let nextVC = self.storyboard?.instantiateViewController(identifier: "Certification") as? JoinViewController
+        let nextVC = self.storyboard?.instantiateViewController(identifier: "Certification") as? CertificateViewController
         nextVC!.modalPresentationStyle = .fullScreen
         self.present(nextVC!, animated: true)
     }
-
-    @IBAction func enterPwBtnTap(_ sender: Any) {
-        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "EnterPw") as? EnterPwViewController
-        nextVC!.username = nameTextField.text!
-        nextVC!.phoneNumber = phoneNumberTextField.text!
-        nextVC!.modalPresentationStyle = .fullScreen
-        self.present(nextVC!, animated: true)
-    }
-
 
 }
